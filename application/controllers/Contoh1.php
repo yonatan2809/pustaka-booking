1c937f01dc24e83c38ae74cc28d113150de014a2
yonatan2809 <verdikaefraim12@gmail.com>
2022-03-23 15:46:47 +0700

    pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Yonatan Arif
+        Saya tinggal di daerah Bekasi
+        Olah raga yang saya sukai adalah Bulutangkis";
+    }
+}
